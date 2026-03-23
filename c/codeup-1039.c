370f1cb310e49fb3bcd77114481fc6002f2ef20c
bellround <rainarmy11@gmail.com>
2026-03-24 07:36:15 +0900

    codeup explanation

---
+++ c/codeup-1039.c
@@ -0,0 +1,10 @@
+// 1039 : [기초-산술연산] 정수 2개 입력받아 합 출력하기2(설명)
+#include <stdio.h>
+int main()
+{
+    long long int a, b;
+    scanf("%lld",&a);
+    scanf("%lld",&b);
+    printf("%lld",a +b);
+    return 0;
+}
